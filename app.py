c26787aeb6063a7a04738c8c095540f07ce4d07f
varun1508singh <varun1508singh@gmail.com>
2021-02-26 22:39:06 +0800

    Added app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api
+from flask_mongoengine import MongoEngine
+from flask import jsonify
+from flask_pymongo import pymongo
+from flask.json import JSONEncoder
+
+app = Flask(__name__)
+
+client = pymongo.MongoClient("mongodb+srv://algorand:algorand@cluster0.v886j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.algorand
+
+import resources
+
+api = Api(app)
+api.add_resource(resources.get_bond_data, '/bondForm')
