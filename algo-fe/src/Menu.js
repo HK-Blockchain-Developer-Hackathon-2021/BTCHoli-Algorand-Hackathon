46b52d3bf7a7a6253192e55983a1e73a36de5b76
Pranav Talwar <talwar.pranav123@gmail.com>
2021-02-28 12:46:42 +0800

    added bg

--- algo-fe/src/Menu.js
+++ algo-fe/src/Menu.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import bg from './bg.jpg'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,7 +22,7 @@ export default function Menu(props) {
 
 
   return (
-    <div>
+    <div style= {{ backgroundImage: `url(${bg})`, backgroundSize: 'cover'}} >
         <Paper variant="outlined" square elevation={10} />
         <Grid
             container
